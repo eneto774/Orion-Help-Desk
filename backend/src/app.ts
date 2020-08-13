95c2801b760b2040a04ccd0096e94178d6c6a856
Edgar Ribeiro <eneto774@gmail.com>
2020-08-12 23:57:12 -0300

    Added Swagger Docummentation for APIs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,16 @@
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
 import './database';
 import AppError from './errors/AppError';
 import routes from './routes';
+import * as swaggerDocument from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   if (err instanceof AppError) {
